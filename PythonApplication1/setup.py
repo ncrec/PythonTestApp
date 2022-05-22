0bcdef00b04aed597950f80c4e31001a1423115f
unknown <danil-kuptsov@yandex.ru>
2022-05-22 22:26:27 +0300

    test

---
+++ PythonApplication1/setup.py
@@ -0,0 +1,13 @@
+import os
+from setuptools import setup, find_packages
+
+setup(name="ncrec_test",
+      version="1.0.0",
+      python_requires='>=3.4',
+      description="Test API client",
+      author="Ncrec",
+      author_email="danil-kuptsov@yandex.ru",
+      packages=find_packages(exclude=["*tests.*", "*test.*"]),
+      license='MIT License',
+      keywords="ncrec",
+      zip_safe=True)
